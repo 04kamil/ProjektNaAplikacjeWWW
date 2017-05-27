acb8a4e9629204b0bf3d5d4f2c7b2068ccceece9
Kamil Brozek <04kamil@gmail.com>
2017-05-27 16:55:32 +0200

    Added email send feature

--- ProjektNaAplikacjeWWW/Controllers/UserController.cs
+++ ProjektNaAplikacjeWWW/Controllers/UserController.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using ProjektNaAplikacjeWWW.DAL;
+using ProjektNaAplikacjeWWW.Models;
+using System.Net.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace ProjektNaAplikacjeWWW.Controllers
 {
@@ -15,10 +19,53 @@ namespace ProjektNaAplikacjeWWW.Controllers
             return View();
         }
 
-        public async Task<ActionResult> Registration()
+        public  ActionResult Registration()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Registration(User u)
+        {
+            u.AccountType = 0;
+            u.Active = false;
+            await UserRepository.Create(u);
+            return Redirect("Create");
+        }
+
+
+        //EmailControl
+
+        public static void SendMail(string userEmail, string content)
+        {
+
+            var fromAdress = new MailAddress("projectappweb@gmail.com", "Ebook Shop App");
+            var toAdress = new MailAddress(userEmail);
+            //tu wpisac poprawne haslo
+            string pass = "";
+
+
+
+
+            MailMessage mail = new MailMessage("projectappweb@gmail.com", userEmail);
+            mail.Subject = "witaj";
+            mail.Body = content;
+
+
+
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(fromAdress.Address, pass);
+
+
+            client.Send(mail);
+        }
+
 
         
     }
--- ProjektNaAplikacjeWWW/DAL/UserRepository.cs
+++ ProjektNaAplikacjeWWW/DAL/UserRepository.cs
@@ -1,11 +1,72 @@
-﻿using System;
+﻿using ProjektNaAplikacjeWWW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace ProjektNaAplikacjeWWW.DAL
 {
-    public class UserRepository
+    public static class UserRepository
     {
+        //Create
+
+        public static async Task Create(User u)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                db.Users.Add(u);
+                await db.SaveChangesAsync();
+            }
+        }
+
+
+        //Read(all)
+        public static List<User> ReadAll()
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                return db.Users.ToList();
+            }
+        }
+
+        public static User Read(Guid id)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                User a = db.Users.Find(id);
+                return a;
+            }
+        }
+
+
+        //Update
+        public static async Task Update(User NewUser)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                User OldUser = db.Users.Find(NewUser.UserID);
+                OldUser.FirstName = NewUser.FirstName;
+                OldUser.LastName = NewUser.LastName;
+                OldUser.Email = NewUser.Email;
+                OldUser.Phone = NewUser.Phone;
+                OldUser.Login = NewUser.Login;
+                await db.SaveChangesAsync();
+
+            }
+        }
+
+        //Delete
+        public static async Task Delete(Guid id)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                db.Users.Remove((from p in db.Users where p.UserID == id select p).SingleOrDefault());
+                await db.SaveChangesAsync();
+            }
+        }
+
+
+
     }
 }
